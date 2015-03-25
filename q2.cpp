8753f73ab7f3788521804793a09e6da75df43007
servin1 <osvaldo_elo@hotmail.com>
2015-03-25 00:13:06 -0600

    #include <iostream> using namespace std; void altura(int){   for(i=1;i<=h;i++)      {        cout<<"T";       }        void veces(){         for(j=1;j<=i;j++)           {                 cout<<<altura();            }       } }  int main (){  return 0; }#include <iostream> using namespace std; void altura(int){   for(i=1;i<=h;i++)      {        cout<<"T";       }        void veces(){         for(j=1;j<=i;j++)           {                 cout<<<altura();            }       } }  int main (){  return 0; }

---
+++ q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void altura(int){
+  for(i=1;i<=h;i++)
+     {
+
+      cout<<"T";
+      }
+
+      void veces(){
+        for(j=1;j<=i;j++)
+          {
+                cout<<<altura();
+           }
+      }
+}
+
+int main (){
+
+return 0;
+}
